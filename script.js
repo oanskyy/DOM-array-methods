f7a238b4e2f68eaa445e60e10abf96812c264305
codessbabe <oana.anghelachi@gmail.com>
2020-05-25 09:24:10 +0200

    Generate random users with fetch API with Async/Await

---
+++ script.js
@@ -0,0 +1,39 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionaires = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+
+// initialize an array where to put all of the ppl, array of objects
+let data = []; 
+
+// Fetch random user and add money
+// function getRandomUser() { 
+//   fetch('https://randomuser.me/api')
+//     .then(res => res.json())
+//     .then(data)
+// }
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+async function getRandomUser() { 
+  const res = await fetch('https://randomuser.me/api'); 
+  const data = await res.json(); 
+
+  const user = data.results[0]; 
+
+  const newUser = { 
+    name: `${user.name.first} ${user.name.last}`, 
+    money: Math.floor(Math.random() * 1000000)
+  };
+
+  addData(newUser);
+}
+
+function addData(obj) { 
+  data.push(obj);
+}
